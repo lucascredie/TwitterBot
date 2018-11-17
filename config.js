aa5ca732f4331eeab685bfa84c65edee4d073930
CredieNogueiraDeLima <uh6456jv@uh6456jvm7.workstations.winona.edu>
2018-11-17 15:14:00 -0600

    added ProcFile

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key:         'VwMFN4SLmKDrLMHmXco9bwypS',
+    consumer_secret:      '7ZBIUyrR5L7iMuoQuWbaZ83ZiNIash8HchrsnFrepEZ5WCE1af',
+    access_token:         '1062089405484187648-mkhmo3DyZu1P0Ububz8ljY5WN3GU6j',
+    access_token_secret:  'QuIBlyQU6z8sv8qMomNmit2MPEcYhOnyFaQg5MB2ZYsEA'
+}
